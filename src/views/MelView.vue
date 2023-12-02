58de9844e199a7903a35d76c688ca07cf402b873
张浩阳 <haoyangmob@163.com>
2023-12-03 01:23:50 +0800

    added dot for path

--- src/views/MelView.vue
+++ src/views/MelView.vue
@@ -9,48 +9,48 @@ const melSpectrograms = [
     id: 1,
     title: '吹气声',
     description: 'fuuuuu的吹气声音，可以看出整体是平滑连续的，但随着人的气剩余量会有渐减、不稳定的趋势',
-    imageUrl: '/mel_images/吹气/露珀塔2_segment_6_mel_spectrogram.png'
+    imageUrl: './mel_images/吹气/露珀塔2_segment_6_mel_spectrogram.png'
   },
   {
     id: 2,
     title: '心跳声',
     description: '可以看出是十分规律的心跳，每次跳动会有快速到达顶峰的声音',
-    imageUrl: '/mel_images/心音/狸酱2_segment_1030_mel_spectrogram.png'
+    imageUrl: './mel_images/心音/狸酱2_segment_1030_mel_spectrogram.png'
   },
   {
     id: 3,
     title: '梵天（鹅毛棒）',
     description: '刺啦刺啦~的密集的鹅毛棒刮擦耳壁的声音，由于鹅毛丝其实是一簇一簇并不是完全整体，加上人手动转动的不稳定，所以呈现这样的规律',
-    imageUrl: '/mel_images/梵天/chloe1_左梵天4_mel_spectrogram.png'
+    imageUrl: './mel_images/梵天/chloe1_左梵天4_mel_spectrogram.png'
   },
   {
     id: 4,
     title: '水瓶摇晃',
     description: '有空气空腔的水瓶在翻转时的声音，气泡会在翻转后快速上浮，发出渐快的 嘟嘟嘟 的声音',
-    imageUrl: '/mel_images/水声/是Sumimi呀1_segment_15_mel_spectrogram.png'
+    imageUrl: './mel_images/水声/是Sumimi呀1_segment_15_mel_spectrogram.png'
   },
   {
     id: 5,
     title: '泡沫洗头',
     description: '在理发店有人给自己沾着泡沫按摩头皮的声音，规律的波动是手指抓头的节奏',
-    imageUrl: '/mel_images/泡沫洗头/狸酱1_segment_241_mel_spectrogram.png'
+    imageUrl: './mel_images/泡沫洗头/狸酱1_segment_241_mel_spectrogram.png'
   },{
     id: 6,
     title: '精油揉耳',
     description: '手上带着精油，慢慢按摩揉搓耳廓的声音，由于一个行为单元相对长，而且耳朵的形状是非对称非循环的，所以频谱图也很有特色',
-    imageUrl: '/mel_images/精油按摩/第二颗纽扣给樱酱3_segment_239_mel_spectrogram.png'
+    imageUrl: './mel_images/精油按摩/第二颗纽扣给樱酱3_segment_239_mel_spectrogram.png'
   },
   {
     id: 7,
     title: '直升机（快速掏耳）',
     description: '正如名字，声音会像直升机螺旋桨一样急促密集且高低清晰',
-    imageUrl: '/mel_images/直升机/小英1_segment_38_mel_spectrogram.png'
+    imageUrl: './mel_images/直升机/小英1_segment_38_mel_spectrogram.png'
   },
   {
     id: 8,
     title: '轻语',
     description: '人说话的声音，所以并不是非常规律，声音很轻，会尽量减少喉咙发音而多使用口腔发音',
-    imageUrl: '/mel_images/轻语/梵拉1_segment_234_mel_spectrogram.png'
+    imageUrl: './mel_images/轻语/梵拉1_segment_234_mel_spectrogram.png'
   },
   
   // 更多 mel-spectrogram 数据
