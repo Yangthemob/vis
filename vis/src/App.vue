fce5b6e5c69fd31e4d6c92a9655cee4e1ca043dd
张浩阳 <haoyangmob@163.com>
2023-12-02 17:55:43 +0800

    some pages

--- vis/src/App.vue
+++ vis/src/App.vue
@@ -1,85 +1,37 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="bg-black text-white">
+    <nav class="container mx-auto p-4 flex justify-between items-center">
+      <div class="flex-1">
+        <!-- 左侧：Home链接 -->
+        <RouterLink 
+          to="/" 
+          class="text-xl font-semibold hover:bg-white hover:text-black transition-colors duration-300 rounded-md p-2"
+        >
+          Home
+        </RouterLink>
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <div class="flex-1 text-right">
+        <!-- 右侧：其他链接 -->
+        <RouterLink 
+          to="/mel" 
+          class="text-xl font-semibold hover:bg-white hover:text-black transition-colors duration-300 mx-2 rounded-md p-2"
+        >
+          Mel-Spectrogram
+        </RouterLink>
+        <RouterLink 
+          to="/statistics" 
+          class="text-xl font-semibold hover:bg-white hover:text-black transition-colors duration-300 mx-2 rounded-md p-2"
+        >
+          Statistics
+        </RouterLink>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- vis/src/views/HomeView.vue
+++ vis/src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// 引入ImageCard组件
+import ImageCard from '@/components/ImageCard.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="bg-white text-black">
+
+    <!-- 第一部分：项目介绍 -->
+    <section class="container mx-auto p-6 flex flex-wrap items-center mt-10">
+      <div class="flex-1">
+        <h1 class="text-4xl font-bold mb-2">
+          Trigger Sound/ASMR  Visualization
+        </h1>
+        <p class="text-lg mb-4">
+          触发音(Trigger Sound)与自发性知觉经络反应(Autonomous Sensory Meridian Response)类音声的可视化
+        </p>
+      </div>
+      <div class="flex-1">
+        <!-- 在这里插入图片，可以是本地或远程图片 -->
+        <img src="path-to-your-image.jpg" alt="项目图片" class="rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"/>
+      </div>
+    </section>
+
+    <!-- 第二部分：图片卡片展示 -->
+    <section class="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <!-- 这里可以通过循环来展示多个卡片，以下是一个示例卡片 -->
+      <ImageCard
+        title="卡片标题"
+        description="这里是关于这个卡片的详细介绍。"
+        imageUrl="path-to-your-image.jpg"
+      />
+    </section>
   </main>
 </template>
