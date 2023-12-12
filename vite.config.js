fcee86f26e3af1b0e89183056fe217c5b7c2d666
张浩阳 <haoyangmob@163.com>
2023-12-12 20:51:05 +0800

    modify base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/vis/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/vis/' : '/',
+  base: '/',
   plugins: [
     vue(),
   ],
